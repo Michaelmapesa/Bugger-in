7c4055225f3fc12c76bd873330aa7e898f142b75
michael mapesa <michaelmapesa93@gmail.com>
2022-01-16 15:31:59 +0300

    creat pizza object

---
+++ index.js
@@ -0,0 +1,20 @@
+var price, crust_price, topping_price;
+let total=0;
+class pizza{
+    constructor(name,size,crust,topping,total){
+        this.name=name;
+        this.size=size;
+        this.crust=crust;
+        this.topping=topping;
+        this.total=total;
+    }
+
+}
+$(document).ready(function(){
+    $("#button1").click(function(event){
+        let name1=$("#name").val();
+        let size1=$("#size").val();
+        let crust1=$("#crust").val();
+        let topping1=[];
+    })
+})
