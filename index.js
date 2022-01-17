992440f3baf10c89615c92ceb7ae132670b208b4
michael mapesa <michaelmapesa93@gmail.com>
2022-01-17 18:26:31 +0300

    remove console log tester

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
    $.each($("input[name='toppings']:checked"), function(){            
        ptopping.push($(this).val());
    });
-   console.log(ptopping.join(", "));
+   
 
    switch(psize){
     case "0":
@@ -28,17 +28,17 @@ $(document).ready(function(){
     break;
     case "large":
        price = 1500;
-       console.log(price);
+      
      break;
      case "medium":
        price = 900;
-       console.log("The price is "+price);
+       
      break;
      case "small":
        price = 700;
-       console.log(price);
+       
      default:
-       console.log("error"); 
+       
    }
    switch(pcrust){
       case "0":
@@ -54,13 +54,12 @@ $(document).ready(function(){
         crust_price = 200;
       break;
       default:
-        console.log("No price"); 
+
     }
     let topping_value = ptopping.length*50;
-    console.log("toppins value" + topping_value);
+    
 
     if((psize == "0") && (pcrust == "0")){
-      console.log("nothing selected");
       $("#btn").show();
       $("#information").show();
       $("div.choise").hide();
@@ -75,7 +74,6 @@ $(document).ready(function(){
     }
 
     total = price + crust_price + topping_value;
-    console.log(total);
     let checkoutTotal =0;
     checkoutTotal = checkoutTotal + total;
 
@@ -94,24 +92,24 @@ $(document).ready(function(){
       $.each($("input[name='toppings']:checked"), function(){            
           ptopping.push($(this).val());
       });
-      console.log(ptopping.join(", "));
+  
       switch(psize){
         case "0":
           price =0;
         break;
         case "large":
            price = 1500;
-           console.log(price);
+           
          break;
          case "medium":
            price = 900;
-           console.log("The price is "+price);
+           
          break;
          case "small":
            price = 700;
-           console.log(price);
+        
          default:
-           console.log("error"); 
+           
        }
        switch(pcrust){
           case "0":
@@ -127,23 +125,18 @@ $(document).ready(function(){
             crust_price = 200;
           break;
           default:
-            console.log("No price"); 
+            
         }
         let topping_value = ptopping.length*50;
-        console.log("toppins value" + topping_value);
         total = price + crust_price + topping_value;
-        console.log(total);
 
         checkoutTotal = checkoutTotal + total;
-        console.log(checkoutTotal);
+      
       // function
       var newOrder = new Getpizza(pname, psize, pcrust,ptopping,total);
 
       $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
-      console.log(newOrder);
       
-      
-
     });
     // Check btn
     $("button#check").click(function(){ 
@@ -151,7 +144,6 @@ $(document).ready(function(){
       $("button#add").hide();
       $("button.delivery").show();
       $("#delivery").slideDown(1000);
-      console.log("Your total bills is sh. "+checkoutTotal);
       $("#pizzatotal").append("Your bill is sh. "+checkoutTotal);
     });
 
@@ -165,7 +157,6 @@ $(document).ready(function(){
       $("#pizzatotal").hide();
       $("#delivery").hide();
       let deliceryamount= checkoutTotal+150;
-      console.log("You will pay sh. "+deliceryamount+" on delivery");
       $("#totalbill").append("Your bill plus delivery fee is: "+deliceryamount);
     });
 
@@ -178,7 +169,6 @@ $(document).ready(function(){
       $("button#sub").hide();
       $(".delivery").hide();
       let deliceryamount= checkoutTotal+150;
-      console.log("Final Bill is: "+deliceryamount);
       let person = $("input#name").val();
       let phone = $("input#phone").val();
       let location = $("input#location").val();
