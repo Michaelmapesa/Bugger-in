b7e58415b42d4a1898b545f63fdfd8231baaa6be
michael mapesa <michaelmapesa93@gmail.com>
2022-01-17 13:34:29 +0300

    checkout button

--- index.js
+++ index.js
@@ -84,50 +84,107 @@ $(document).ready(function(){
         $("#pizzatopping").html(topping1.join(", "));
         $("#total").html(total);
 
-        
-        
+        $("#add").click(function(){
+            let name1=$("#name");
+            let size1=$("#size");
+            let crust1=$("#crust");
+            let topping1=[];
+            $.each($("input[name='topping']:checked"),function(){
+                topping1.push($(this).val());
+            });
+            console.log(topping1.join(", "));
+            switch(size1){
+                case "0":
+               break;
+                case "large":
+                    price=1500;
+                    console.log(price);
+                   break;
+                   case "medium":
+                       price=900;
+                       console.log("price");
+                       break;
+                       case "small":
+                           price=700;
+                           console.log(price);
+                           default:
+                               console.log("error,try solve!");
+            }
 
+            switch(crust1){
+                case "0":
+                    crust_price=0;
+                    break;
+                    case "Crispy":
+                        crust_price=250;
+                        break;
+                        case "Stuffed":
+                            crust_price=150;
+                            break;
+                            case "Gluten-free":
+                                crust_price=200;
+                                break;
+                                default:
+                                    console.log("price unavailable");
+            }
+            let topping_value=topping1.length*50;
+            console.log("your topping value"+topping_value);
+            total=crust_price+price+topping_value;
+            console.log(total);
 
-
-        confirmTotal=confirmTotal+total;
+            confirmTotal=confirmTotal+total;
         console.log(confirmTotal);
 
         var addOrder = new pizza(name1,size1,crust1,topping1,total);
         $("#list1").append('<ul><li id="pizzaname">'+addOrder.name+'<ul><li id="pizzasize">'+addOrder.size+'<ul><li id="pizzacrust">'+addOrder.crust+'<ul><li id="pizzatopping">'+addOrder.topping+'<ul><li id="totals">'+addOrder.total+'</li></ul>')
         console.log(addOrder);
+
+        });
+
+        $("#check").click(function(){
+            $("#check").hide();
+            $("#add").hide();
+            $("#delivery").show();
+            $("#addedprice").show();
+            console.log("Your bill amount to sh." + confirmTotal);
+            $("#pizzatotal").append("pay sh."+confirmTotal);
+
+    
+        });
+    
+        $("#delivery").click(function(){
+            $("#list1").hide();
+            $(".h5").hide();
+            $(".delivery").show();
+            $("#add").hide();
+            $("#pizzatotal").hide();
+            $("#sub").hide();
+            
+            let costDelivery=confirmTotal+250;
+            console.log("Pay: "+costDelivery+"on delivery");
+            $("#totalbill").append("Your bill inclusion of delivery fee is"+costDelivery);
+
+        
     }); 
     
-    $("#check").click(function(){
-        $("#check").hide();
-        $("#add").hide();
-        $("#delivery").show();
-        $("#addedprice").show();
-        console.log("Your bill amount to sh." + confirmTotal);
-
-    });
-
-    $("#delivery").click(function(){
-        $("#list1").hide();
-        $(".h5").hide();
-        $("#sub").hide();
-        let costDelivery=confirmTotal+250;
-        console.log("Pay: "+costDelivery);
-        let location=$("#location").val();
-        let person=$("#name").val;
-        let phone=$("#phone").val;
-
-        if ($("#name").val() && $("#phone").val() && ("#location").val()!=""){
-           $("#finalmessage").append(person+",Thank you,your order will be delivered at " +location+ ". Prepare sh. "+costDelivery);  
-           $("#totalbill").hide();
-           $("finalmessage").show();
-        }
-        else{
-            alert("You have to fill delivery details");
-            $(".delivery").show();
-            $("#sub").show();
-        }
+    let location=$("#location").val();
+            let person=$("#name").val;
+            let phone=$("#phone").val;
+    
+            if ($("#name").val() && $("#phone").val() && ("#location").val()!=""){
+               $("#finalmessage").append(person+",Thank you,your order will be delivered at " +location+ ". Prepare sh. "+costDelivery);  
+               $("#totalbill").hide();
+               $("finalmessage").show();
+            }
+            else{
+                alert("You have to fill delivery details");
+                $(".delivery").show();
+                $("#sub").show();
+            }
+            
+    
+        });
         
-
-    });
+    
     
 });
